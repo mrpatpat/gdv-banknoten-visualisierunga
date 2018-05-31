9e9ea7e41570325944a8e68c52149dde4420dbe0
mrpatpat <adrian.endrich@googlemail.com>
2018-05-31 18:42:55 +0200

    simple coins chart

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import {LineChartSample} from "./line-chart-sample/line-chart-sample";
+import {CoinsLineChartSample} from "./coins-line-chart-sample/coins-line-chart-sample";
 
-const chart = new LineChartSample(800,600, 20);
+const chart = new CoinsLineChartSample(800,600, 50);
 chart.render("body");
