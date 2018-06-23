7dbece79ef3ec18b05c569fc7d908c7f8d0c0d09
mrpatpat <adrian.endrich@googlemail.com>
2018-06-23 18:52:45 +0200

    epoch filter working with zoom

--- src/coins-line-chart-sample/coin-scatter.ts
+++ src/coins-line-chart-sample/coin-scatter.ts
@@ -9,74 +9,36 @@ export class CoinScatter {
     private static HEIGHT = 600 - 2* CoinScatter.MARGIN;
     private static DOT_WIDTH = 32;
 
-    private svg;
+    private container;
     private xScale;
     private yScale;
     private xAxis;
     private yAxis;
+    private zoom;
 
     constructor(private selector: string, private data: CoinRow[] = []) {
 
         this.update = this.update.bind(this);
-        this.zoom = this.zoom.bind(this);
+        this.updateDomains= this.updateDomains.bind(this);
+        this.onZoom = this.onZoom.bind(this);
         DataService.data$.subscribe(data => this.update(data));
 
-        // SVG
-        this.svg = d3
-            .select(selector)
-            .append("svg")
-            .attr("width", CoinScatter.WIDTH + CoinScatter.MARGIN + CoinScatter.MARGIN)
-            .attr("height", CoinScatter.HEIGHT + CoinScatter.MARGIN + CoinScatter.MARGIN)
-            .call(d3.zoom().on("zoom", this.zoom))
-            .append("g")
-            .attr("transform", "translate(" + CoinScatter.MARGIN + "," + CoinScatter.MARGIN + ")");
+        this.container = this.initContainer(selector);
+        this.xScale = this.initScaleX();
+        this.yScale = this.initScaleY();
+        this.xAxis = this.initAxisX();
+        this.yAxis = this.initAxisY();
 
-        // Scale methods
+        this.initAxisLabels();
+        this.initReferenceLine();
 
-        this.xScale = d3
-            .scaleTime()
-            .range([0, CoinScatter.WIDTH])
-            .domain(d3.extent(this.data, (d: CoinRow) => {
-                return +d.von
-            }));
+        this.updateDomains([]);
+        this.update([]);
 
-        this.yScale = d3
-            .scaleLog()
-            .base(10)
-            .range([CoinScatter.HEIGHT, 0])
-            .domain(d3.extent(this.data, (d: CoinRow) => {
-                return +d.euro
-            }));
+    }
 
-        //  Axis
-
-        this.xAxis = this.svg.append("g")
-            .attr("transform", "translate(0," + CoinScatter.HEIGHT + ")")
-            .attr("class", "x-axis")
-            .call(d3.axisBottom(this.xScale));
-
-        this.yAxis = this.svg.append("g")
-            .attr("class", "y-axis")
-            .call(d3.axisLeft(this.yScale));
-
-        //labels
-        this.svg.append("text")
-            .attr("x", CoinScatter.WIDTH / 2)
-            .attr("y",CoinScatter.HEIGHT +20)
-            .attr("dy", "1em")
-            .style("text-anchor", "middle")
-            .text("Jahr");
-
-        this.svg.append("text")
-            .attr("transform", "rotate(-90)")
-            .attr("y", 0 - CoinScatter.MARGIN)
-            .attr("x",0 - (CoinScatter.HEIGHT / 2))
-            .attr("dy", "1em")
-            .style("text-anchor", "middle")
-            .text("Kaufkraft in Euro pro Mark");
-
-        //Ref Line
-        this.svg.append('line')
+    private initReferenceLine() {
+        this.container.append('line')
             .style('stroke', '#900')
             .style('stroke-width', '1px')
             .attr("class", "ref-line")
@@ -84,39 +46,79 @@ export class CoinScatter {
             .attr('y1', this.yScale(1))
             .attr('x2', CoinScatter.WIDTH)
             .attr('y2', this.yScale(1));
+    }
 
-        // dots
-        this.svg.selectAll("circle")
-            .data(data)
-            .enter()
-            .append('image')
-            .attr("xlink:href", d => d.image)
-            .attr("width", CoinScatter.DOT_WIDTH)
-            .attr("x", (d: CoinRow) => {
-                return this.xScale(d.von);
-            })
-            .attr("y", (d: CoinRow) => {
-                return this.yScale(d.euro);
-            })
-            .on("mouseover", DataService.hover)
-            .on("mouseout", ()=>DataService.hover(null));
+    private initAxisLabels() {
+        this.container.append("text")
+            .attr("x", CoinScatter.WIDTH / 2)
+            .attr("y", CoinScatter.HEIGHT + 20)
+            .attr("dy", "1em")
+            .style("text-anchor", "middle")
+            .text("Jahr");
+
+        this.container.append("text")
+            .attr("transform", "rotate(-90)")
+            .attr("y", 0 - CoinScatter.MARGIN)
+            .attr("x", 0 - (CoinScatter.HEIGHT / 2))
+            .attr("dy", "1em")
+            .style("text-anchor", "middle")
+            .text("Kaufkraft in Euro pro Mark");
+    }
+
+    private initAxisY() {
+        return this.container.append("g")
+            .attr("class", "y-axis")
+            .call(d3.axisLeft(this.yScale));
+    }
+
+    private initAxisX() {
+        return this.container.append("g")
+            .attr("transform", "translate(0," + CoinScatter.HEIGHT + ")")
+            .attr("class", "x-axis")
+            .call(d3.axisBottom(this.xScale));
+    }
+
+    private initScaleY() {
+        return d3
+            .scaleLog()
+            .base(10)
+            .range([CoinScatter.HEIGHT, 0]);
+    }
+
+    private initScaleX() {
+        return d3
+            .scaleTime()
+            .range([0, CoinScatter.WIDTH]);
+    }
+
+    private initContainer(selector: string) {
+
+        this.zoom = d3.zoom().on("zoom", this.onZoom);
+
+        return d3
+            .select(selector)
+            .append("svg")
+            .attr("width", CoinScatter.WIDTH + CoinScatter.MARGIN + CoinScatter.MARGIN)
+            .attr("height", CoinScatter.HEIGHT + CoinScatter.MARGIN + CoinScatter.MARGIN)
+            .call(this.zoom)
+            .append("g")
+            .attr("transform", "translate(" + CoinScatter.MARGIN + "," + CoinScatter.MARGIN + ")");
 
     }
 
-    public zoom() {
-        // re-scale y axis during zoom; ref [2]
-        this.xAxis.call(d3.axisBottom(this.xScale).scale(d3.event.transform.rescaleX(this.xScale)));
-        this.yAxis.call(d3.axisLeft(this.yScale).scale(d3.event.transform.rescaleY(this.yScale)));
+    public onZoom() {
 
-        // re-draw circles using new y-axis scale; ref [3]
         let new_yScale = d3.event.transform.rescaleY(this.yScale);
         let new_xScale = d3.event.transform.rescaleX(this.xScale);
 
-        this.svg.select('.ref-line')
+        this.xAxis.call(d3.axisBottom(this.xScale).scale(new_xScale));
+        this.yAxis.call(d3.axisLeft(this.yScale).scale(new_yScale));
+
+        this.container.select('.ref-line')
             .attr('y1',new_yScale(1))
             .attr('y2',new_yScale(1));
 
-        this.svg.selectAll("image")
+        this.container.selectAll("image")
             .attr("y", function(d) { return new_yScale(+d.euro); })
             .attr("x", function(d) { return new_xScale(+d.von); })
             .attr("width", CoinScatter.DOT_WIDTH * d3.event.transform.k);
@@ -124,45 +126,34 @@ export class CoinScatter {
 
     public async update(data: CoinRow[]) {
 
-        this.data = data;
+        this.resetZoom();
+        this.updateDomains(data);
+        this.updateAxis();
+        this.updateReferenceLine();
+        this.updateDots(data);
+    }
 
-        //Update axis domains
-        this.xScale.domain(d3.extent(data, (d: CoinRow) => {
-            return +d.von
-        })).nice();
-
-        this.yScale.domain(d3.extent(data, (d: CoinRow) => {
-            return +d.euro
-        })).nice();
-
-        //axis
-        this.svg.select(".x-axis")
+    private resetZoom() {
+        d3.select("svg")
             .transition()
             .duration(1000)
-            .call(d3.axisBottom(this.xScale));
+            .call(this.zoom.translateTo, CoinScatter.WIDTH/2 + CoinScatter.MARGIN, CoinScatter.HEIGHT/2 + CoinScatter.MARGIN)
+            .call(this.zoom.scaleTo, 1)
+            .on("end", ()=>{
+                this.zoom.transform(d3.select("svg"), d3.zoomIdentity.scale(1) );
+            });
+    }
 
-        this.svg.select(".y-axis")
-            .transition()
-            .duration(1000)
-            .call(d3.axisLeft(this.yScale));
+    private updateDots(data: CoinRow[]) {
+        let dots = this.container.selectAll("image").data(data);
 
-        //Ref Line
-        this.svg.select('.ref-line')
-            .transition()
-            .duration(1000)
-            .attr('y1', this.yScale(1))
-            .attr('y2', this.yScale(1));
-
-        //dots
-        let circle = this.svg.selectAll("image").data(data);
-
-        circle
+        dots
             .enter()
             .append('image')
-            .attr("xlink:href", d => d.image)
+            .attr("xlink:href", d => d.thumb)
             .attr("width", CoinScatter.DOT_WIDTH)
             .on("mouseover", DataService.hover)
-            .on("mouseout", ()=>DataService.hover(null))
+            .on("mouseout", () => DataService.hover(null))
             .transition()
             .duration(1000)
             .attr("x", (d: CoinRow) => {
@@ -172,9 +163,7 @@ export class CoinScatter {
                 return this.yScale(d.euro);
             });
 
-        circle
-            .on("mouseover", DataService.hover)
-            .on("mouseout", ()=>DataService.hover(null))
+        dots
             .transition()
             .duration(1000)
             .attr("x", (d: CoinRow) => {
@@ -184,13 +173,41 @@ export class CoinScatter {
                 return this.yScale(d.euro);
             });
 
-        circle
+        dots
             .exit()
             .transition()
             .duration(1000)
             .attr("width", 0)
             .remove();
+    }
 
+    private updateReferenceLine() {
+        this.container.select('.ref-line')
+            .transition()
+            .duration(1000)
+            .attr('y1', this.yScale(1))
+            .attr('y2', this.yScale(1));
+    }
+
+    private updateAxis() {
+        this.container.select(".x-axis")
+            .transition()
+            .duration(1000)
+            .call(d3.axisBottom(this.xScale));
+
+        this.container.select(".y-axis")
+            .transition()
+            .duration(1000)
+            .call(d3.axisLeft(this.yScale));
+    }
+
+    private updateDomains(data: CoinRow[]) {
+        this.xScale.domain(d3.extent(data, (d: CoinRow) => {
+            return +d.von
+        }));
+        this.yScale.domain(d3.extent(data, (d: CoinRow) => {
+            return +d.euro
+        }));
     }
 
 }
